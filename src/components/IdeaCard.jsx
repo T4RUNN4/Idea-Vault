fd5fa902e3a5ff0526b1629ecf86fc133963038d
Tarunna Sen <tarunnasen@gmail.com>
2026-05-22 01:02:18 +0600

    Delete and Update idea (UI)

--- src/components/IdeaCard.jsx
+++ src/components/IdeaCard.jsx
@@ -1,7 +1,13 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { Trash } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 export default function IdeaCard({ idea }) {
+  const pathname = usePathname();
+
   return (
     <div className="card bg-base-200 border border-base-300 shadow-sm rounded-2xl px-4 py-4">
       <figure className="relative rounded-xl">
@@ -9,8 +15,8 @@ export default function IdeaCard({ idea }) {
       </figure>
 
       <div className="card-body p-5">
-        <div className="flex items-start justify-between gap-3">
-          <h2 className="card-title text-base-content text-lg font-semibold mt-4">
+        <div className="flex items-start justify-between  mt-4 gap-3">
+          <h2 className="card-title text-base-content text-lg font-semibold">
             {idea.title}{" "}
             {idea.isTrending && (
               <span className="badge bg-red-600 text-white text-xs px-3 py-1 shrink-0">
@@ -18,16 +24,28 @@ export default function IdeaCard({ idea }) {
               </span>
             )}
           </h2>
+          {pathname === "/my-ideas" && (
+            <button className="btn btn-ghost btn-sm rounded-md text-gray-500 hover:text-red-500 transition">
+              <Trash size={18} />
+            </button>
+          )}
         </div>
 
         <p className="text-sm text-base-content/70">{idea.shortDescription}</p>
 
-        <Link
-          href={`/ideas/${idea._id}`}
-          className="btn bg-lime-500 rounded-md text-white font-medium text-sm hover:bg-lime-600 transition mt-2"
-        >
-          View details →
-        </Link>
+        <div className="card-actions flex gap-2 mt-6">
+          <Link
+            href={`/ideas/${idea._id}`}
+            className="btn bg-lime-500 rounded-md text-white font-medium text-sm hover:bg-lime-600 transition "
+          >
+            View details →
+          </Link>
+          {pathname === "/my-ideas" && (
+            <button className="btn btn-ghost rounded-md text-white font-medium text-sm ">
+              Update
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
