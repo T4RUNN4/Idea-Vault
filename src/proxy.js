d924cf4568478ed0022c158cc04fca548808dbdd
Tarunna Sen <tarunnasen@gmail.com>
2026-05-21 18:16:23 +0600

    Restricted login and register for logged in user

--- src/proxy.js
+++ src/proxy.js
@@ -7,13 +7,30 @@ export async function proxy(request) {
     headers: await headers(),
   });
 
-  if (!session) {
+  const pathName = request.nextUrl.pathname;
+
+  if (
+    !session &&
+    (pathName.startsWith("/add-ideas") ||
+      pathName.startsWith("/my-ideas") ||
+      pathName.startsWith("/my-interactions"))
+  ) {
     const loginUrl = new URL("/login", request.url);
     loginUrl.searchParams.set("redirect", request.nextUrl.pathname);
     return NextResponse.redirect(loginUrl);
   }
+
+  if (session && (pathName.startsWith("/login") || pathName.startsWith("/register"))) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
 }
 
 export const config = {
-  matcher: ["/add-ideas", "/my-ideas", "/my-interactions"],
+  matcher: [
+    "/add-ideas",
+    "/my-ideas",
+    "/my-interactions",
+    "/login",
+    "/register",
+  ],
 };
