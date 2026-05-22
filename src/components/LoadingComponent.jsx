655ba476751848ad31a25b76347655cda5adebfe
Tarunna Sen <tarunnasen@gmail.com>
2026-05-22 12:57:14 +0600

    Removed unused loading component

--- src/components/LoadingComponent.jsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingComponent() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <span className="loading loading-spinner loading-lg"></span>
-    </div>
-  );
-}
